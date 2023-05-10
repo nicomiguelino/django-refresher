444a0e046c11c0a2a85e2cc1c2b9e9f8c5dad994
nicomiguelino <nicomiguelino2014@gmail.com>
2023-05-09 22:28:56 -0700

    Write Docker files for production as well (WIP).

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,9 +5,27 @@ set -euo pipefail
 ./manage.py migrate
 ./manage.py seed
 
-sqlite_web \
-    --host 0.0.0.0 \
-    --port 8001 \
-    db.sqlite3 &
-
-./manage.py runserver 0.0.0.0:8000
+# parse optional command line arguments here
+# e.g. --dev for development mode
+# e.g. --prod for production mode
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --dev)
+            echo "Running in development mode"
+            ./manage.py runserver 0.0.0.0:8000
+            sqlite_web \
+                --host 0.0.0.0 \
+                --port 8001 \
+                db.sqlite3 &
+            ;;
+        --prod)
+            echo "Running in production mode"
+            ./manage.py runserver 0.0.0.0:4000
+            ;;
+        *)
+            echo "Unknown option: $key"
+            exit 1
+            ;;
+    esac
+done
