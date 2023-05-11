d6b8e582823dd16f068213c92fdb659f81a79363
nicomiguelino <nicomiguelino2014@gmail.com>
2023-05-10 17:11:47 -0700

    Make use of `.env` files.

--- django_refresher/settings.py
+++ django_refresher/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+cehv)(=s@pjh8cn4crx8-&v_(vc%=d*@fdzsn$*czqpw=85*_'
+SECRET_KEY = environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
